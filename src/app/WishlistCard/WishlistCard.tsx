5915d639aa009b550dd5462b874ddac324de8390
tomaCezar1 <cezar.toma1@gmail.com>
2021-05-05 01:30:55 +0300

    delete implementation nearly done

--- src/app/WishlistCard/WishlistCard.tsx
+++ src/app/WishlistCard/WishlistCard.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import Time from '@material-ui/icons/AccessTime';
 import Close from '@material-ui/icons/Close';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles({
     root: {
@@ -23,8 +29,30 @@ const useStyles = makeStyles({
     },
 });
 
-function Wishlistcard({ title, date }): JSX.Element {
+function Wishlistcard({ title, id, date }): JSX.Element {
     const classes = useStyles();
+
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleDeleteRequest = () => {
+        fetch(`http://localhost:8080/api/wishlists/${id}`, {
+            method: 'DELETE',
+        })
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+            .catch((e) => {
+                throw new Error(e);
+            });
+    };
+
     return (
         <Card className={`${classes.root} card`}>
             <div className="card-top">
@@ -36,11 +64,29 @@ function Wishlistcard({ title, date }): JSX.Element {
             </div>
             <div className="button-container">
                 <button className="card-btn primary-btn">Add</button>
-                <button className="card-btn grey-btn">Edit</button>
+                <button className="card-btn grey-btn" onClick={handleClickOpen}>
+                    Edit
+                </button>
             </div>
-            <i className="card-delete-btn">
+            <i className="card-delete-btn" onClick={handleClickOpen}>
                 <Close />
             </i>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle>Are you sure you want to delete this wishlist?</DialogTitle>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleDeleteRequest} color="primary" autoFocus>
+                        Continue
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Card>
     );
 }
