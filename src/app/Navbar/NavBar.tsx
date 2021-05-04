bb6365953f1720f97d3dc1ef55c92c73e996dfd5
tomaCezar1 <cezar.toma1@gmail.com>
2021-05-04 15:05:54 +0300

    added new wishlist button on dashboard

--- src/app/Navbar/NavBar.tsx
+++ src/app/Navbar/NavBar.tsx
@@ -1,19 +1,37 @@
+import { useState } from 'react';
+
 import Settings from '@material-ui/icons/SettingsOutlined';
 import Notifications from '@material-ui/icons/NotificationsOutlined';
 import { Avatar } from '@material-ui/core';
 
-export default function NavBar() {
+function NavBar(): JSX.Element {
+    const [showForm, setShowForm] = useState(false);
+
+    const toggleForm = () => {
+        setShowForm(true);
+    };
+
     return (
         <nav className="navbar">
             <div className="main-heading">
                 <h3 className="nav-greeting">Welcome back, Guest</h3>
                 <h1 className="nav-title">Your Dashboard</h1>
             </div>
-            <div className="nav-quick-access">
-                <Settings style={{ color: '#8f92a1', fontSize: 24 }} />
-                <Notifications style={{ color: '#8f92a1', fontSize: 24 }} id="notifications-icon" />
-                <Avatar alt="Guest Image" style={{ height: 48, width: 48 }} />
+            <div className="navbar-btn-wrapper">
+                <button className="card-btn primary-btn nav-btn" onClick={toggleForm}>
+                    New Wishlist
+                </button>
+                <div className="nav-quick-access">
+                    <Settings style={{ color: '#8f92a1', fontSize: 24 }} />
+                    <Notifications
+                        style={{ color: '#8f92a1', fontSize: 24 }}
+                        id="notifications-icon"
+                    />
+                    <Avatar alt="Guest Image" style={{ height: 48, width: 48 }} />
+                </div>
             </div>
         </nav>
     );
 }
+
+export default NavBar;
