7644be722f23d9b7772fb31ff6ebcc479c8c867a
tomaCezar1 <cezar.toma1@gmail.com>
2021-05-04 14:08:20 +0300

    display all cards with ssr request

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,5 +1,14 @@
-function Dashboard() {
-    return <section className="dashboard-container"></section>;
+import Wishlistcard from '../WishlistCard/WishlistCard';
+
+function Dashboard({ wishlists }): JSX.Element {
+    return (
+        <section className="dashboard-container">
+            {wishlists.map((wishlist) => {
+                const date: string = wishlist.wishlistDate.split('T')[0];
+                return <Wishlistcard key={wishlist.id} title={wishlist.title} date={date} />;
+            })}
+        </section>
+    );
 }
 
 export default Dashboard;
