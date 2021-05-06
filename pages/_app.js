f727befa3d63e5184f1dca626988e3865f175161
tomaCezar1 <cezar.toma1@gmail.com>
2021-05-07 00:11:10 +0300

    Created theme to avoid layout shift on load

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
-import '../src/styles/globals.scss';
 import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import formReducer from '../src/store/reducers/formReducer';
 
-function MyApp({ Component, pageProps }) {
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/styles/Theme/theme';
+import '../src/styles/globals.scss';
+
+function MyApp(props) {
+    const { Component, pageProps } = props;
+
     React.useEffect(() => {
+        // Remove the server-side injected CSS.
         const jssStyles = document.querySelector('#jss-server-side');
         if (jssStyles) {
             jssStyles.parentElement.removeChild(jssStyles);
@@ -15,10 +25,27 @@ function MyApp({ Component, pageProps }) {
     const store = createStore(formReducer);
 
     return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
+        <>
+            <Head>
+                <title>My page</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </Provider>
+        </>
     );
 }
 
 export default MyApp;
+
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired,
+};
